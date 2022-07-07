3a1cd2a41909cc44e3e7af343e0c6fec6be5cc38
Oleg Naumov <asler-72@mail.ru>
2022-07-07 10:43:22 +0300

    first commit

---
+++ src/app/topPagination/topPagination.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'top-pagination',
+  templateUrl: 'topPagination.component.html',
+  styleUrls: ['topPagination.component.less']
+})
+
+export class topPaginationComponent {}
---
+++ src/app/topPagination/topPagination.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {topPaginationComponent} from "./topPagination.component";
+import {TuiLinkModule, TuiSvgModule} from "@taiga-ui/core";
+
+@NgModule({
+  imports: [CommonModule, TuiLinkModule, TuiSvgModule],
+  exports: [topPaginationComponent],
+  declarations: [topPaginationComponent],
+})
+
+export class topPaginationModule {}
