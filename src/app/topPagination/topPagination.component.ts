ee15cc1a6595d5c329cf0d36fc6bdf64c585f4bf
OlegNau <70605440+OlegNau@users.noreply.github.com>
2022-07-07 11:37:29 +0300

    Delete src/app/topPagination directory

--- src/app/topPagination/topPagination.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'top-pagination',
-  templateUrl: 'topPagination.component.html',
-  styleUrls: ['topPagination.component.less']
-})
-
-export class topPaginationComponent {}
